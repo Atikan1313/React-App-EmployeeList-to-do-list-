9b2db48d55db79a9e66f9052baa762e297ff5732
Light ice <light.icce@gmail.com>
2025-11-25 21:26:49 +0500

    Подъём состояний

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,16 +2,17 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 
 import './employees-list.css';
 
-const EmployeesList = ({data, onDelete}) => {
+const EmployeesList = ({data, onDelete, onToggleProp}) => {
 
     const elements = data.map(item => {
         const {id, ...itemProps} = item;
         return (
             <EmployeesListItem 
-            key={id} 
-            name={itemProps.name} 
-            salary={itemProps.salary}
-            onDelete={() => onDelete(id)}/>
+                key={id} 
+                {...itemProps}
+                onDelete={() => onDelete(id)}
+                onToggleProp={(e)=>onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))}
+            />
         )
     })
 
